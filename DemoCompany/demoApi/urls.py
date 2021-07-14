2e0d3f427e7fd18ad8d6ecc2e0c691fec5c2d0f1
”Lev” <vinnipyx38@gmail.com>
2021-07-14 17:27:03 +0500

    added openapi

--- DemoCompany/demoApi/urls.py
+++ DemoCompany/demoApi/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework.schemas import get_schema_view
 from .views import *
 
 app_name = 'demoApi'
 urlpatterns = [
+    path('openapi/', get_schema_view(
+        title="Project for XsollaSchool",
+        description="API for all things",
+        version="1.0.0"
+    ), name='openapi-schema'),
     path('product/create/', ProductCreateView.as_view()),
     path('product/all/', ProductsListView.as_view()),
     path('product/detail/<int:pk>/', ProductDetailView.as_view())
