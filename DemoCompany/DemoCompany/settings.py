42866a868ec40dddbe7daac79de78fbd79f34205
”Lev” <vinnipyx38@gmail.com>
2021-07-15 12:38:23 +0500

    pre-deployment training

--- DemoCompany/DemoCompany/settings.py
+++ DemoCompany/DemoCompany/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure--#@ushv!*lpr@k+@tdqviyggjamcbsi&%$u7gafgo@!l$92uq3'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dasdasdasdasfgadsfasfasfhgdsangasdasdo-insecure--#@ushv!*lpr@k+@tdqviyggjamcbsi&%$u7gafgo@!l$92uq3')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dasdasdasdasfgadsfasfnjasfhgdsangasdasdo-insecure--#@ushv!*lpr@k+@tdqviyggjamcbsi&%$u7gafgo@!l$92uq3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
